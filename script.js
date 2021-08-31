6b035f3d7d3501a2907243446d38c2ddf1cd8925
Tevissaur <berserkboar@protonmail.com>
2021-08-31 14:07:57 -0500

    cleaned up code

--- script.js
+++ script.js
@@ -1,50 +1,17 @@
 
-/*
-
-*/
 let movieBaseUrl = 'https://movie-database-imdb-alternative.p.rapidapi.com/?r=json' // Append queries to this string
-let foodBaseUrl = ''
+let foodBaseUrl = 'https://www.themealdb.com/api/json/v2/'
+let testUrl = foodBaseUrl + mealKey + '/latest.php'
 
 
-/*
-MOVIE API:
-=============================================================================
-Fetch should look like this
-    fetch("https://movie-database-imdb-alternative.p.rapidapi.com/?r=json", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
-            "x-rapidapi-key": "0b3ba113c5msh66e12ed828c15d9p196e46jsna2205c88742e"
-        }
-    }) 
-
-
-FOOD API:
-=============================================================================
-Fetch should look like this
-    fetch("https://tasty.p.rapidapi.com/recipes/list?from=0&size=20", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "tasty.p.rapidapi.com",
-		"x-rapidapi-key": "0b3ba113c5msh66e12ed828c15d9p196e46jsna2205c88742e"
-	}
-})
-*/
-fetch(movieBaseUrl,
-    {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
-            "x-rapidapi-key": "0b3ba113c5msh66e12ed828c15d9p196e46jsna2205c88742e"
-        }
-    })
+fetch(testUrl)
     .then(response => {
-        return response.json()
+        console.log(response);
+        return response.json();
     })
-    .then(data => {
-        showData(data)
+    .then( data => {
+        console.log(data);
     })
-
-function showData(data) {
-    console.log(data)
-}
+    .catch(err => {
+        console.error(err);
+    });
