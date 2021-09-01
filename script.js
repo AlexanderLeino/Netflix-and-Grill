1d7081256042d5fed8e9c505fef6ab5a8eded87c
Tevissaur <berserkboar@protonmail.com>
2021-09-01 08:39:17 -0500

    testing API calls out

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 
 let movieBaseUrl = 'https://movie-database-imdb-alternative.p.rapidapi.com/?r=json' // Append queries to this string
 let foodBaseUrl = 'https://www.themealdb.com/api/json/v2/'
-let testUrl = foodBaseUrl + mealKey + '/latest.php'
+let testUrl = foodBaseUrl + mealKey + '/random.php'
+// let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+// for (let i in alphabet) {
+//     let loopUrl = testUrl + alphabet[i]
+//     fetch(loopUrl)
+//     .then(response => {
+//         console.log(response);
+//         return response.json();
+//     })
+//     .then( data => {
+//         console.log(data);
+//         localStorage.setItem(`Meals letter ${alphabet[i]} search: `, JSON.stringify(data))
+//     })
+//     .catch(err => {
+//         console.error(err);
+//     });
+// }
 
+let pasta = []
+for (let i = 0; i < 20; i++) {
 
-fetch(testUrl)
-    .then(response => {
-        console.log(response);
-        return response.json();
-    })
-    .then( data => {
-        console.log(data);
-    })
-    .catch(err => {
-        console.error(err);
-    });
+    fetch(testUrl)
+        .then(response => {
+            console.log(response);
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            randomArray.push(data)
+            localStorage.setItem('Random', JSON.stringify(randomArray))
+        })
+        .catch(err => {
+            console.error(err);
+        });
+
+}
