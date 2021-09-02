d1d99512760f5e10839eb407fda17880323b6f7e
Tevissaur <berserkboar@protonmail.com>
2021-09-01 19:52:31 -0500

    added button

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,42 @@
 
-let movieBaseUrl = 'https://movie-database-imdb-alternative.p.rapidapi.com/?r=json' // Append queries to this string
-let foodBaseUrl = 'https://www.themealdb.com/api/json/v2/'
+let movieBaseUrl = 'https://unogsng.p.rapidapi.com/' // Append queries to this string
+let movieFetchObj = {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "unogsng.p.rapidapi.com",
+        "x-rapidapi-key": rapidAPIKey
+    }
+}
+let foodBaseUrl = `https://www.themealdb.com/api/json/v2/${mealDbKey}/`
+let button = document.getElementById('send-call')
+button.addEventListener('click', function (e) {
+    e.preventDefault()
+    let url = movieBaseUrl + 'search?genrelist=794&type=movie&orderby=rating&audiosubtitle_andor=and&limit=100&subtitle=english&countrylist=78&offset=0'
+    fetch(url, movieFetchObj)
+        .then(response => {
+            console.log(trackAPICalls(response))
+            return response.json()
+        })
+        .then(data => {
+            console.log(data)
+        })
+})
 
 
-class meals {
-    constructor(category, mealsList) {
-        this.category = category
-        this.mealsList = mealsList
+function trackAPICalls(response) {
+    return response.headers.get("x-ratelimit-requests-remaining")
+}
+
+class userSuggestions {
+    constructor(genre, recipes) {
+        this.genre = genre
+        this.recipes = recipes
     }
     getMealsList() {
-        return this.mealsList
+        return this.recipes
     }
     getCategory() {
-        return this.category
+        return this.genre
     }
 }
 
